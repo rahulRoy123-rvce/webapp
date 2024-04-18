3df7bbb6deb41ba6b751189b74a32f7020d74f47
Yajush Mishra <yajushmishra8@gmail.com>
2024-04-19 01:20:59 +0530

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,84 +1,105 @@
 // BannerCard.jsx
-import React from 'react';
-import CabonCalc from '../Assets/CarbonCalc.png';
-import Projects from '../Assets/Projects.png';
-import Trackhere from '../Assets/Trackhere.png';
+import React from "react";
+import CabonCalc from "../Assets/CarbonCalc.png";
+import Projects from "../Assets/Projects.png";
+import Trackhere from "../Assets/Trackhere.png";
 
 const BannerCard = () => {
   return (
-    <div className="bg-[#fdfaec]">
-       <h1 className="text-3xl font-bold text-[#1A5130] mb-3">
-          Decarbonn your Business<br/>
-          <span className="text-3xl text-[#1A5130]">Invest in Carbon Offset project</span>
+    <div className="bg-[#fdfaec] w-full">
+      <div className="p-12">
+        <h1 className="text-[#1a5130] text-6xl font-bold mb-20 ">
+          Decarbonn your Business
+          <br />
+          <span className="text-[#1a5130] text-6xl font-bold">
+            Invest in Carbon Offset project
+          </span>
         </h1>
-    <div className="max-w mx-auto p-4 rounded-lg  flex flex-col md:flex-row items-center">
-      <div className="md:w-1/2">
-        <img
-          src={CabonCalc}
-          alt="Industrial smokestacks emitting smoke"
-          className="w-full h-auto object-cover rounded-lg"
-        />
+        {/* */}
+        <div
+          className="border border-black rounded-xl w-full flex flex-col group "
+          style={{ height: "620px" }}
+        >
+          <div className="max-w mx-auto  rounded-lg  flex flex-col md:flex-row items-center">
+            <div
+              className="md:w-2/3 h-full overflow-hidden rounded-lg"
+              style={{ height: "620px" }}
+            >
+              <img
+                src={CabonCalc}
+                alt="Industrial smokestacks emitting smoke"
+                className="w-full h-full object-cover rounded-2xl transition-all duration-700 group-hover:scale-110"
+              />
+            </div>
+            <div className="md:w-1/2 p-4 flex flex-col gap-5">
+              <h2 className="text-5xl font-bold text-[#1A5130] mb-2 text-left">
+                CALCULATE YOUR CARBON FOOTPRINTS
+              </h2>
+              <p className="mb-5 text-left text-3xl text-black">
+                Use our open-source calculator to
+                <br /> find out how much CO2 your
+                <br /> business emits.
+              </p>
+              <button className="text-white bg-[#1A5130] transition-colors duration-300 hover:bg-white hover:text-[#1a5130] hover:border hover:border-[#1a5130] font-medium py-2 px-6 rounded-full w-72 h-12">
+                Carbon Calculator
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="md:w-1/2 p-4">
-       
-        
-        <h2 className="text-5xl font-bold text-[#1A5130] mb-2 text-left">
-          CALCULATE YOUR CARBON FOOTPRINTS
-        </h2>
-        <p className="mb-5 text-left text-3xl text-black">
-          Use our open-source calculator to<br/> find out how much CO2 your<br/> business emits.
-        </p>
-        <button className="text-white bg-[#1A5130] hover:bg-green-700 font-medium py-2 px-6 rounded-xl">
-          Carbon Calculator
-        </button>
+
+      <div className="p-12">
+        <div
+          className="border border-black rounded-xl w-full flex  group "
+          style={{ height: "620px" }}
+        >
+          <div className="md:flex-1 md:pr-10 md:w-1/2">
+            <h2 className="text-5xl font-bold text-[#1A5130] mb-2 text-left">
+              CALCULATE YOUR CARBON FOOTPRINTS
+            </h2>
+            <p className="mb-5 text-left text-3xl text-black">
+              Use our open-source calculator to find out how much CO2 your
+              business emits.
+            </p>
+            <button className="text-white bg-[#1A5130] hover:bg-green-750 font-medium py-2 px-6 rounded-xl">
+              Carbon Calculator
+            </button>
+          </div>
+          <div
+            className="md:w-[750px] h-full overflow-hidden rounded-lg"
+            style={{ height: "620px" }}
+          >
+            <img
+              src={Projects}
+              alt="Industrial smokestacks emitting smoke"
+              className="w-full h-full object-cover rounded-2xl transition-all duration-700 group-hover:scale-110"
+            />
+          </div>
+        </div>
       </div>
-      
+
+      <div className="max-w mx-auto p-4 rounded-lg  flex flex-col md:flex-row items-center">
+        <div className="md:w-1/2">
+          <img
+            src={Trackhere}
+            alt="Industrial smokestacks emitting smoke"
+            className="w-full h-auto object-cover rounded-lg"
+          />
+        </div>
+        <div className="md:w-1/2 p-4">
+          <h2 className="text-5xl font-bold text-[#1A5130] mb-2 text-left">
+            CALCULATE YOUR CARBON FOOTPRINTS
+          </h2>
+          <p className="mb-5 text-left text-3xl text-black">
+            Use our open-source calculator to find out how much CO2 your
+            business emits.
+          </p>
+          <button className="text-white bg-[#1A5130] hover:bg-green-700 font-medium py-2 px-6 rounded-xl">
+            Carbon Calculator
+          </button>
+        </div>
+      </div>
     </div>
-    <div className=" p-5 rounded-lg  flex flex-col md:flex-row items-center justify-between">
-      <div className="md:flex-1 md:pr-10">
-        
-        <h2 className="text-5xl font-bold text-[#1A5130] mb-2 text-left">
-          CALCULATE YOUR CARBON FOOTPRINTS
-        </h2>
-        <p className="mb-5 text-left text-3xl text-black">
-          Use our open-source calculator to find out how much CO2 your business emits.
-        </p>
-        <button className="text-white bg-[#1A5130] hover:bg-green-700 font-medium py-2 px-6 rounded-xl">
-          Carbon Calculator
-        </button>
-      </div>
-      <div className="md:flex-1">
-        <img
-          src={Projects}
-          alt="Industrial smokestacks emitting smoke"
-          className="w-full h-auto object-cover rounded-lg"
-        />
-      </div>
-    </div>
-    <div className="max-w mx-auto p-4 rounded-lg  flex flex-col md:flex-row items-center">
-      <div className="md:w-1/2">
-        <img
-          src={Trackhere}
-          alt="Industrial smokestacks emitting smoke"
-          className="w-full h-auto object-cover rounded-lg"
-        />
-      </div>
-      <div className="md:w-1/2 p-4">
-        
-        <h2 className="text-5xl font-bold text-[#1A5130] mb-2 text-left">
-          CALCULATE YOUR CARBON FOOTPRINTS
-        </h2>
-        <p className="mb-5 text-left text-3xl text-black">
-          Use our open-source calculator to find out how much CO2 your business emits.
-        </p>
-        <button className="text-white bg-[#1A5130] hover:bg-green-700 font-medium py-2 px-6 rounded-xl">
-          Carbon Calculator
-        </button>
-      </div>
-      
-    </div>
-   
-   </div>
   );
 };
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import logo from '../Assets/deCarbonn_logo.png';
+import React from "react";
+import logo from "../Assets/deCarbonn_logo.png";
 
-import Instagram from '../UI/Instagram';
-import Whatsapp from '../UI/Whatsapp';
-import Facebook from '../UI/Facebook';
-import X from '../UI/X';
+import Instagram from "../UI/Instagram";
+import Whatsapp from "../UI/Whatsapp";
+import Facebook from "../UI/Facebook";
+import X from "../UI/X";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#133009] text-white">
+    <footer className="bg-[#1a5130] text-white mb-0 h-[480px]">
       <div className="max-w-6xl mx-auto  py-10">
-        <div className="flex flex-wrap justify-between items-center">
-        
+        <div className="flex flex-wrap justify-between items-center mt-28">
           <div className="mb-6 w-full md:w-auto md:mb-0">
-            <a href="#" className="flex items-center justify-center  space-x-2 mb-4">
-              <img src={logo} alt="DeCarbonn Logo " className="h-14" />
-              
+            <a
+              href="#"
+              className="flex items-center justify-center  space-x-2 mb-4"
+            >
+              <img src={logo} alt="DeCarbonn Logo " className="h-20 w-72" />
             </a>
             <div className="flex flex-col">
               <p className="mb-4">Have any query?</p>
@@ -25,42 +26,76 @@ const Footer = () => {
                   placeholder="write your query here..."
                   className="p-2 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-700 focus:ring-white"
                 />
-                <button className="bg-white text-green-700 px-4 rounded-md font-bold">Send</button>
+                <button className="bg-white text-green-700 px-4 rounded-md font-bold">
+                  Send
+                </button>
               </div>
             </div>
             <div className="flex space-x-4 mb-6 md:mb-0 my-4">
-              <a href="#" className="hover:text-gray-300"><Instagram /></a>
-              <a href="#" className="hover:text-gray-300"><Whatsapp /></a>
-              <a href="#" className="hover:text-gray-300"><Facebook /></a>
-              <a href="#" className="hover:text-gray-300"><X /></a>
+              <a href="#" className="hover:text-gray-300">
+                <Instagram />
+              </a>
+              <a href="#" className="hover:text-gray-300">
+                <Whatsapp />
+              </a>
+              <a href="#" className="hover:text-gray-300">
+                <Facebook />
+              </a>
+              <a href="#" className="hover:text-gray-300">
+                <X />
+              </a>
             </div>
           </div>
 
-          
           <div className="flex flex-wrap md:space-x-20 mb-6 md:mb-0">
             <div className="mb-6 md:mb-0">
               <h3 className="font-bold mb-2">COMPANY</h3>
               <ul>
-                <li><a href="#" className="hover:text-gray-300">Home</a></li>
-                <li><a href="#" className="hover:text-gray-300">Project</a></li>
-                <li><a href="#" className="hover:text-gray-300">About</a></li>
+                <li>
+                  <a href="#" className="hover:text-gray-300">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-300">
+                    Project
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-300">
+                    About
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <h3 className="font-bold mb-2">INITIATIVES</h3>
               <ul>
-                <li><a href="#" className="hover:text-gray-300">Maiti Labs</a></li>
-                <li><a href="#" className="hover:text-gray-300">DeCarbonn</a></li>
-                <li><a href="#" className="hover:text-gray-300">GreenBit</a></li>
+                <li>
+                  <a href="#" className="hover:text-gray-300">
+                    Maiti Labs
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-300">
+                    DeCarbonn
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-300">
+                    GreenBit
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
         </div>
 
-       
         <div className="flex flex-col items-center">
           <p className="text-center py-2">Made with ❤️ at Maiti Labs</p>
-          <p>Copyright © 2024 <strong>DeCarbonn</strong></p>
+          <p>
+            Copyright © 2024 <strong>DeCarbonn</strong>
+          </p>
         </div>
       </div>
     </footer>
